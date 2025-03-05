7ebbbf1faa25abe85567622de616a73fa43480fc
Jack Dench <jdench1989@gmail.com>
2025-03-05 17:14:01 +0000

    Fixed deliberate failure

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 def test_get_home(web_client):
     response = web_client.get("/")
     assert response.status_code == 200
-    assert response.data.decode("utf-8") == "Hello, world!"
+    assert response.data.decode("utf-8") == "I am a CI-CD hero!"
 
 def test_get_greet(web_client):
     response = web_client.get("/greet/testname")
