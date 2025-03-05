a3e3f50e1a617ca509c2623d0b98248638a2f4a0
Jack Dench <jdench1989@gmail.com>
2025-03-05 16:52:06 +0000

    Fixed the deliberate failure

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Sausage"
+    return "Hello, world!"
   
 @app.route('/greet/<string:name>', methods=['GET'])
 def greet(name):
